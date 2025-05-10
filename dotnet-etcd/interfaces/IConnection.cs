afd389892431eb86107cf8fa59d5437a2c704759
Shubham Ranjan <shubham.ranjan1804@gmail.com>
2025-05-10 17:07:51 +0530

    nit: cleanup

--- dotnet-etcd/interfaces/IConnection.cs
+++ dotnet-etcd/interfaces/IConnection.cs
@@ -1,6 +1,3 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
 using Etcdserverpb;
 using V3Electionpb;
 using V3Lockpb;
